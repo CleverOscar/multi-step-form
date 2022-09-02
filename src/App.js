9926293cbf66216da315fd26bd5450b994679039
cleveroscar <Oscarpa9188@gmail.com>
2022-09-01 21:41:14 -0500

    Basic App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import CancelForm from "./CancelForm/CancelForm";
+
+function App() {
+  return (
+    <div >
+      <header >
+        Multi Step Form Guide
+      </header>
+
+
+      <div>
+        <CancelForm/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CancelForm/CancelForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function CancelForm(){
+    return(
+        <div>
+            Cancel Form
+        </div>
+    )
+}
