cfe461018efd97c1bafc7b890e50cabea48c9cf3
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2022-09-03 15:30:20 -0500

    Basic Form Created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import CancelForm from "./CancelForm/CancelForm";
 function App() {
   return (
     <div >
-      <header >
-        Multi Step Form Guide
-      </header>
-
-
-      <div>
         <CancelForm/>
-      </div>
     </div>
   );
 }
--- src/CancelForm/CancelForm.jsx
+++ src/CancelForm/CancelForm.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import './FormStyles.css'
 
 
 export default function CancelForm(){
     return(
-        <div>
-            Cancel Form
+        <div className='CancelForm '>
+
+            <p> Image goes Here</p>
+
+            <div className='offerDescription'>
+                <p className='boldFont before'>Before You Go...</p>
+                <p className='boldFont help'>Would 50% OFF For 6  Months Help?</p>
+
+                <p>We know things REALLY suck in the world right now - and many businesses and freelancers are struggling to stay afloat during this COVID-19 crisis</p>
+
+                <p className='boldFont'>So if you could use the extra cushion, grab 50% off for 6 months.</p>
+
+                <p>We hope you're staying safe and healthy!</p>
+
+                <div className='offers'>
+                    <button className='acceptOffer'>
+                        50% Off for 6 months
+                    </button>
+
+                    <button className='cancelSub'>
+                        No, thanks! I'll cancel!
+                    </button>
+                </div>
+
+            </div>
         </div>
     )
 }
